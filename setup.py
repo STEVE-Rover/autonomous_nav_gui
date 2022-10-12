f410bab16bbc320085ad2fe896b84d1df7d1b2bd
Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
2022-10-11 22:43:42 -0400

    Changed name and fixed plugin discovery issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
   
 d = generate_distutils_setup(
-    packages=['autonomous_nav_gui'],
+    packages=['rqt_autonomous_nav'],
     package_dir={'': 'src'},
 )
 
--- src/rqt_autonomous_nav/autonomous_nav_module.py
+++ src/rqt_autonomous_nav/autonomous_nav_module.py
@@ -6,21 +6,33 @@ from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget
 
-class AutonomousNavGuiPlugin(Plugin):
+class MyPlugin(Plugin):
 
     def __init__(self, context):
-        super(AutonomousNavGuiPlugin, self).__init__(context)
+        super(MyPlugin, self).__init__(context)
         # Give QObjects reasonable names
-        self.setObjectName('Autonomous Nav GUI')
+        self.setObjectName('MyPlugin')
+
+        # Process standalone plugin command-line arguments
+        from argparse import ArgumentParser
+        parser = ArgumentParser()
+        # Add argument(s) to the parser.
+        parser.add_argument("-q", "--quiet", action="store_true",
+                      dest="quiet",
+                      help="Put plugin in silent mode")
+        args, unknowns = parser.parse_known_args(context.argv())
+        if not args.quiet:
+            print 'arguments: ', args
+            print 'unknowns: ', unknowns
 
         # Create QWidget
         self._widget = QWidget()
         # Get path to UI file which should be in the "resource" folder of this package
-        ui_file = os.path.join(rospkg.RosPack().get_path('autonomous_nav_gui'), 'resource', 'autonomous_nav_gui.ui')
+        ui_file = os.path.join(rospkg.RosPack().get_path('rqt_autonomous_nav'), 'resource', 'autonomous_nav_gui.ui')
         # Extend the widget with all attributes and children from UI file
         loadUi(ui_file, self._widget)
         # Give QObjects reasonable names
-        self._widget.setObjectName('AutonomousNavPluginUi')
+        self._widget.setObjectName('MyPluginUi')
         # Show _widget.windowTitle on left-top of each plugin (when 
         # it's set in _widget). This is useful when you open multiple 
         # plugins at once. Also if you open multiple instances of your 

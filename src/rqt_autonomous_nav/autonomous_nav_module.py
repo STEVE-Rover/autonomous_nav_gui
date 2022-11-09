85892975fc853efc80e551981f411f52de1c0c36
Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
2022-11-09 11:42:07 -0500

    import and export list

--- src/rqt_autonomous_nav/autonomous_nav_module.py
+++ src/rqt_autonomous_nav/autonomous_nav_module.py
@@ -1,10 +1,10 @@
 import os
 import rospy
 import rospkg
-
+import csv
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
-from python_qt_binding.QtWidgets import QWidget
+from python_qt_binding.QtWidgets import QWidget, QFileDialog, QListWidgetItem
 
 class MyPlugin(Plugin):
 
@@ -22,8 +22,8 @@ class MyPlugin(Plugin):
                       help="Put plugin in silent mode")
         args, unknowns = parser.parse_known_args(context.argv())
         if not args.quiet:
-            print 'arguments: ', args
-            print 'unknowns: ', unknowns
+            print('arguments: ', args)
+            print('unknowns: ', unknowns)
 
         # Create QWidget
         self._widget = QWidget()
@@ -43,6 +43,12 @@ class MyPlugin(Plugin):
         # Add widget to the user interface
         context.add_widget(self._widget)
 
+        self.goal_list = []
+
+        # Connect buttons
+        self._widget.importListButton.clicked.connect(self.import_list) 
+        self._widget.exportListButton.clicked.connect(self.export_list)
+
     def shutdown_plugin(self):
         # TODO unregister all publishers here
         pass
@@ -61,3 +67,50 @@ class MyPlugin(Plugin):
         # Comment in to signal that the plugin has a way to configure
         # This will enable a setting button (gear icon) in each dock widget title bar
         # Usually used to open a modal configuration dialog
+
+    def import_list(self):
+        response = QFileDialog.getOpenFileName(
+            parent = self._widget,
+            caption = "Select file",
+            directory = os.getcwd(),
+            filter = "Goal list file (*.csv)"
+        )
+
+        file = response[0]
+        print(file)
+
+        self.goal_list = []
+        with open(file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=",")
+            for i, row in enumerate(csv_reader):
+                if (i != 0):
+                    self.goal_list.append(row)
+        print(self.goal_list)
+        self.show_goal_list()
+
+    def show_goal_list(self):
+        self._widget.goalListWidget.clear()
+        for i, row in enumerate(self.goal_list):
+            text = '[' + str(i) + '] ' + ' '.join(row)
+            print(text)
+            self._widget.goalListWidget.addItem(QListWidgetItem(text))
+
+    def export_list(self):
+        response = QFileDialog.getSaveFileName(
+            parent = self._widget,
+            caption = "Save file",
+            directory = os.getcwd(),
+            filter = "Goal list file (*.csv)"
+        )
+
+        file = response[0]
+        if not file.endswith(".csv"):
+            file = file + ".csv"
+        print(file)
+
+        with open(file, mode='w') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',')
+            csv_writer.writerow(['type', 'latitude', 'longitude'])
+            for row in self.goal_list:
+                csv_writer.writerow(row)
+
